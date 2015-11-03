b8cbfc51682bc5b11aee358e7d70354a3b813b33
hyunjik11 <hyunjik11@gmail.com>
2015-11-03 00:54:09 +0000

    fixed synthdata for gp

--- SynthDataExperiment.jl
+++ SynthDataExperiment.jl
@@ -1,7 +1,8 @@
-#using GaussianProcess
-#using GPT_SGLD
-#using Distributions
+using GaussianProcess
+using GPT_SGLD
+using Distributions
 
+if 1==0
 sigmaRBF=1.4; sigma=0.2;
 f=SECov(sigmaRBF,1)
 gp=GP(0,f,4)
@@ -11,8 +12,7 @@ x2=rand(Uniform(30,100),N);
 x3=rand(Uniform(1000,1020),N);
 x4=rand(Uniform(50,100),N);
 X=[x1 x2 x3 x4];
-y=GPrand(gp,X)+sigma*randn(N);
-Xtrain = X[1:Ntrain,1:D]; ytrain = y[1:Ntrain];
+Xtrain = X[1:Ntrain,1:D];
 XtrainMean=mean(Xtrain,1); 
 XtrainStd=Array(Float64,1,D);
 for i=1:D
@@ -20,6 +20,9 @@ for i=1:D
 end
 Xtrain = datawhitening(Xtrain);
 Xtest = (X[Ntrain+1:end,1:D]-repmat(XtrainMean,N-Ntrain,1))./repmat(XtrainStd,N-Ntrain,1);
+X=[Xtrain;Xtest];
+y=GPrand(gp,X)+sigma*randn(N);
+ytrain = y[1:Ntrain];
 ytest = y[Ntrain+1:end];
 
 tic()
@@ -27,12 +30,14 @@ gp_post=GPpost(gp,Xtrain,ytrain,0.2);
 gp_pred=Mean(gp_post,Xtest);
 toc()
 println(norm(ytest-gp_pred)/sqrt(N-Ntrain))
+end
 
+#if 1==0
 seed=17;
-n=10;
+n=100;
 phitrain=feature(Xtrain,n,sigmaRBF,seed);
 phitest=feature(Xtest,n,sigmaRBF,seed);
-r=10; Q=100;m=10;eps=0.0001;maxepoch=10;
+r=5; Q=100;m=50;eps=0.01;maxepoch=10;
 tic()
 w_store,U_store,I=GPT_SGLDERM(phitrain,ytrain,sigma,sigmaRBF,r,Q,m,eps,eps,maxepoch);
 toc()
@@ -41,4 +46,4 @@ tic()
 fhat=pred(w_store[:,minind],U_store[:,:,:,minind],I,phitest);
 toc()	
 println(minRMSE)
-
+#end
