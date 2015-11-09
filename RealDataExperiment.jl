0f1a9b946d3d978eb016a44ce6ce187b0209268e
hyunjik11 <hyunjik11@gmail.com>
2015-11-09 20:37:00 +0000

    Wed evening

--- RealDataExperiment.jl
+++ RealDataExperiment.jl
@@ -14,23 +14,31 @@ println("RMSE for GP=",ytrainStd*norm(ytest-gp_pred)/sqrt(N-Ntrain))
 end
 
 if 1==1
-	data=readtable("Folds5x2_pp.csv", header = true);
-	data = convert(Array,data);
-	N=size(data,1); D=4; Ntrain=5000; seed=17; sigmaRBF=1.4332;sigma=0.2299;
-	Xtrain = data[1:Ntrain,1:D]; ytrain = data[1:Ntrain,D+1];
-	XtrainMean=mean(Xtrain,1); 
-	XtrainStd=Array(Float64,1,D);
+@everywhere data=readtable("Folds5x2_pp.csv", header = true);
+@everywhere data = convert(Array,data);
+@everywhere N=size(data,1);
+@everywhere D=4;
+@everywhere Ntrain=5000;
+@everywhere seed=17;
+@everywhere sigmaRBF=1.4332;
+@everywhere sigma=0.2299;
+@everywhere Xtrain = data[1:Ntrain,1:D];
+@everywhere ytrain = data[1:Ntrain,D+1];
+@everywhere XtrainMean=mean(Xtrain,1); 
+@everywhere XtrainStd=Array(Float64,1,D);
 	for i=1:D
 	    XtrainStd[1,i]=std(Xtrain[:,i]);
 	end
-	ytrainMean=mean(ytrain); ytrainStd=std(ytrain);
-	Xtrain = datawhitening(Xtrain); ytrain=datawhitening(ytrain);
-	Xtest = (data[Ntrain+1:end,1:D]-repmat(XtrainMean,N-Ntrain,1))./repmat(XtrainStd,N-Ntrain,1);
-	ytest = (data[Ntrain+1:end,D+1]-ytrainMean)/ytrainStd;
-
-    burnin=17;maxepoch=3;
-t=((50,50),(50,100),(50,150),(50,200),(100,50),(100,100),(100,150),(100,200),(200,50),(200,100),(200,150),(200,200),(400,50),(400,100),(400,150),(400,200), )
-    @parallel for  i=length(t)
+@everywhere ytrainMean=mean(ytrain);
+@everywhere ytrainStd=std(ytrain);
+@everywhere Xtrain = datawhitening(Xtrain);
+@everywhere ytrain=datawhitening(ytrain);
+@everywhere Xtest = (data[Ntrain+1:end,1:D]-repmat(XtrainMean,N-Ntrain,1))./repmat(XtrainStd,N-Ntrain,1);
+@everywhere ytest = (data[Ntrain+1:end,D+1]-ytrainMean)/ytrainStd;
+@everywhere burnin=17;
+@everywhere maxepoch=3;
+@everywhere t=((50,50),(50,100),(50,150),(50,200),(100,50),(100,100),(100,150),(100,200),(200,50),(200,100),(200,150),(200,200),(400,50),(400,100),(400,150),(400,200))
+    @parallel for  i=1:length(t)
         m,n=t[i]
 	phitrain=feature(Xtrain,n,sigmaRBF,seed);
 	phitest=feature(Xtest,n,sigmaRBF,seed);
